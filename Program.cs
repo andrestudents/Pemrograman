1b10a4f1df9607fb1f24379a0655c47a76ee89d8
Andreas <andreassetyo82@gmail.com>
2023-03-19 11:50:49 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp
+{
+
+   
+    internal class Program
+    {
+        static void main(string[] args)
+        {
+          
+
+        }
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Aplikasi Kalkulator";
+
+            Console.Write("\n1. Penjumlahan ");
+            Console.Write("\n2. Pengurangan ");
+            Console.Write("\n3. Perkalian ");
+            Console.Write("\n4. Pembagian ");
+
+            Console.Write("\npilih : ");
+            int pilih = int.Parse(Console.ReadLine());
+
+            Console.Write("Masukkan nilai : ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Masukkan nilai : ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nHasil Penjumlahan " + a + " + " + b + " = " + tambah(a, b));
+            Console.WriteLine("\nTekan bebas untuk keluar");
+            Console.ReadKey();
+        }
+        static int tambah(int a, int b)
+        {
+            return a + b;
+        }
+        static int kurang(int a, int b)
+        {
+            return a - b;
+        }
+        static int kali(int a, int b)
+        {
+            return a * b;
+        }
+        static int bagi(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
